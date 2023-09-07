fe3c0d27ddbc12bc2cd1db036973ebf1b13ad746
Jessie <101580183+Cheanssie@users.noreply.github.com>
2023-09-07 16:41:28 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ctpbaurzkgpc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jessieliew-employee"
+customregion = "us-east-1"
+
